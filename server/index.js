c327c39df3b63d799bd35a3159552054de1c9132
Swiggityy <chris.nguyen@ayli.io>
2020-02-26 17:28:12 -0800

    asdf

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const keys = require("./config/keys");
+const keys = require("./config/keys.js");
 require("./models/User");
-require("./services/passport");
+require("./services/passport.js");
 
 const app = express();
 
